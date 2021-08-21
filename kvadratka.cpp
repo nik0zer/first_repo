88f654b7e5296793eecc0afbf91c7c22a13c11bb
nik0zer <87236151+nik0zer@users.noreply.github.com>
2021-08-21 20:38:07 +0300

    Update kvadratka.cpp

--- kvadratka.cpp
+++ kvadratka.cpp
@@ -62,110 +62,81 @@ float schet_linear(float b, float c)
 */
 void schet_kvadr(float a, float b, float c, float* x1, float* x2)
 {
-    assert(*x1 == NULL);
-    assert(*x2 == NULL);
+    assert(x1 != NULL);
+    assert(x2 != NULL);
+    assert(x2 != x1);
     float d = discr(a, b, c);
     *x1 = (-sqrt(d) - b) / (2 * a);
     *x2 = (sqrt(d) - b) / (2 * a);
 }
 
 /*!
-Решает квадратное уравнение от переданных коэффициентов, может работать в двух режимах: в обычном печатает решение уравнения в консоль, в режиме тестов возвращает строку с количеством корней и их значениями
+\brief Решает квадратное уравнение от переданных коэффициентов
+Решает квадрратное уравнение разбивая на различные случаи в зависимости от входных данных квадратное, линейное и т.д
 \param a,b,с коэффициенты
-\param str возвращаемая в режиме тестов строка
+\param x1,x2 корни возвращаемые по итогам работы
+\warning при бесконечном количестве корней return возвращает 8
+\return количество корней
 */
-void kvadratka(float a, float b, float c, char str[])
+int kvadratka(float a, float b, float c, float* x1, float* x2)
 {
-    float x1 = 0, x2 = 0;
-    if(TEST)
+    assert(x1 != NULL);
+    assert(x2 != NULL);
+    assert(x2 != x1);
+    if(abs(a) < D && abs(b) < D && abs(c) < D)
     {
-        if(abs(a) < D && abs(b) < D && abs(c) < D)
-        {
-            sprintf(str, "8:");
-        }
-        else
-        {
-            if(abs(a) < D && abs(b) < D)
-            {
-                sprintf(str, "0:");
-            }
-            else
-            {
-                if(abs(a) < D)
-                {
-                    sprintf(str, "1:%.4f", schet_linear(b, c));
-                }
-                else
-                {
-                    switch(check(a, b, c))
-                    {
-                    case 0:
-                        sprintf(str, "0:");
-                        break;
-                    case 1:
-                        schet_kvadr(a, b, c, &x1, &x2);
-                        sprintf(str, "%d:%.4f", check(a, b, c), x1);
-                        break;
-                    case 2:
-                        schet_kvadr(a, b, c, &x1, &x2);
-                        sprintf(str, "%d:%.4f %.4f", check(a, b, c), x1, x2);
-                    }
-                }
-            }
-        }
+        return 8;
     }
     else
     {
-        if(abs(a) < D && abs(b) < D && abs(c) < D)
+        if(abs(a) < D && abs(b) < D)
         {
-            puts("roots belongs to R");
+            return 0;
         }
         else
         {
-            if(abs(a) < D && abs(b) < D)
+            if(abs(a) < D)
             {
-                puts("number of roots: 0");
+                *x1 = schet_linear(b, c);
+                return 1;
             }
             else
             {
-                if(abs(a) < D)
-                {
-                    puts("number of roots: 1");
-                    printf("root: %.4f\n", schet_linear(b, c));
-                }
-                else
-                {
-                    printf("number of roots: %d\n", check(a, b, c));
-                    switch(check(a, b, c))
-                    {
-                    case 0:
-                        break;
-                    case 1:
-                        schet_kvadr(a, b, c, &x1, &x2);
-                        printf("root: %.4f\n", x1);
-                        break;
-                    case 2:
-                        schet_kvadr(a, b, c, &x1, &x2);
-                        printf("roots: %.4f %.4f\n", x1, x2);
-                    }
-                }
+                schet_kvadr(a, b, c, x1, x2);
+                return check(a, b, c);
             }
         }
     }
 }
 
 /*!
-тестирующая функция прогоняет значения из data через kvadratka и сравнивает с эталонными из data_check и пишет результаты тестов в консоль
+тестирующая функция прогоняет значения из data через kvadratka, сравнивает с эталонными из data_check и пишет результаты тестов в консоль
 */
 void test()
 {
     int k = 9;
+    float x1, x2;
     float data[k][3]= {{1, 4,-3}, {1, 0, -4}, {0,0,0}, {0,4,5}, {0, 0, 5}, {2, 3, 7}, {15246, 120536, -645721}, {0, 1154526, -1125452}, {1, -2068, 1069156}};
     char data_check[k][50] = {"2:-4.6458 0.6458", "2:-2.0000 2.0000", "8:", "1:-1.2500", "0:", "0:", "2:-11.5675 3.6614", "1:0.9748", "1:1034.0000"};
     for(int i = 0; i < k; i++)
     {
         char str[50];
-        kvadratka(data[i][0], data[i][1], data[i][2], str);
+        int number_of_roots = kvadratka(data[i][0], data[i][1], data[i][2], &x1, &x2);
+        switch(number_of_roots)
+        {
+        case 0:
+            sprintf(str, "0:");
+            break;
+        case 1:
+            sprintf(str, "1:%.4f", x1);
+            break;
+        case 2:
+            sprintf(str, "2:%.4f %.4f", x1, x2);
+            break;
+        case 8:
+            sprintf(str, "8:");
+            break;
+        }
         if(strcmp(data_check[i], str) == 0)
         {
             printf("test %i OK\n", i + 1);
@@ -180,11 +151,11 @@ void test()
 }
 
 /*!
-в режиме теста запускает тестирующую функцию, в обычном считывает коэффициенты из консоли и запускает kvadratka
+в режиме теста запускает тестирующую функцию, в обычном считывает коэффициенты из консоли и запускает kvadratka, а затем печатает полученные значения
 */
 int main()
 {
-    float a = 0, b = 0, c = 0;
+    float a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
     char str[20];
     if(TEST)
     {
@@ -194,7 +165,23 @@ int main()
     {
         puts("type a b c");
         scanf("%f %f %f", &a, &b, &c);
-        kvadratka(a, b, c, str);
+        int number_of_roots = kvadratka(a, b, c, &x1, &x2);
+        if(number_of_roots != 8)
+        {
+            printf("number of roots: %d\n", number_of_roots);
+            if(number_of_roots == 1)
+            {
+                printf("root: %.4f\n", x1);
+            }
+            if(number_of_roots == 2)
+            {
+                printf("roots: %.4f %.4f\n", x1, x2);
+            }
+        }
+        else
+        {
+            puts("roots belongs to R");
+        }
         return 0;
     }
 }
