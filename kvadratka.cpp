fa7fbe3fef877f97cebe229df059b09c092a9363
nik0zer <87236151+nik0zer@users.noreply.github.com>
2021-08-22 14:20:11 +0300

    инициализация массива структур и проверки на переполнение float

--- kvadratka.cpp
+++ kvadratka.cpp
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 #include <assert.h>
-#include <string.h>
+#include <float.h>
+
 
 #define TEST true
-#define NUMBER_OF_TESTS 9
+#define NUMBER_OF_TESTS 11
 #define PRECISION 0.0001
 
-enum rootnum {NO_ROOT, ONE_ROOT, TWO_ROOTS, INFINITE_ROOTS = 8};
+enum rootnum {NO_ROOT, ONE_ROOT, TWO_ROOTS, ERROR_OCCUR, INFINITE_ROOTS = 8};
+enum errors {NO_ERRORS, VARIABLE_OVERFLOW};
+int Errno = 0;
 
 struct test_data
 {
@@ -27,16 +30,6 @@ int floatcmp(float x1, float x2)
     if((x1 - x2) < -PRECISION)
         return -1;
     return 0;
-}
-
-/*!
-\brief Находит дискриминант от переданных коэффициентов
-\param a,b,с коэффициенты
-\return дискриминант
-*/
-float discr(float a, float b, float c)
-{
-    return (b * b) - (4 * a * c);
 }
 
 /*!
@@ -62,7 +55,14 @@ int schet_kvadr(float a, float b, float c, float* x1, float* x2)
     assert(x1 != NULL);
     assert(x2 != NULL);
     assert(x2 != x1);
-    float d = discr(a, b, c);
+    if((abs(FLT_MAX / b) < abs(b)) || (abs((FLT_MAX / 4) / a) < abs(c)))
+    {
+        *x1 = 0;
+        *x2 = 0;
+        Errno = VARIABLE_OVERFLOW;
+        return ERROR_OCCUR;
+    }
+    float d = (b * b) - (4 * a * c);
     if(floatcmp(d, 0) == -1)
     {
         *x1 = 0;
@@ -80,7 +80,7 @@ int schet_kvadr(float a, float b, float c, float* x1, float* x2)
 \detail Решает квадрратное уравнение разбивая на различные случаи в зависимости от входных данных квадратное, линейное и т.д
 \param a,b,с коэффициенты
 \param x1,x2 корни возвращаемые по итогам работы
-\warning при бесконечном количестве корней return возвращает 8
+\warning при бесконечном количестве корней return возвращает 8, при ошибке возвращает 3
 \return количество корней
 */
 int kvadratka(float a, float b, float c, float* x1, float* x2)
@@ -111,51 +111,19 @@ int kvadratka(float a, float b, float c, float* x1, float* x2)
 void test()
 {
     float x1 = 0, x2 = 0;
-    test_data test_data_array[NUMBER_OF_TESTS];
-    test_data_array[0] =
+    test_data test_data_array[NUMBER_OF_TESTS] =
     {
-        {    1,      4,      -3},
-        {2,    -4.6458,  0.6458}
-    };
-    test_data_array[1] =
-    {
-        {1,       0,       -4},
-        {2, -2.0000,   2.0000}
-    };
-    test_data_array[2] =
-    {
-        {0,     0,       0},
-        {8,     0,       0}
-    };
-    test_data_array[3] =
-    {
-        {0,      4,       5},
-        {1,    -1.2500,   0}
-    };
-    test_data_array[4] =
-    {
-        {0,       0,        5},
-        {0,       0,        0}
-    };
-    test_data_array[5] =
-    {
-        {2,         3,         7},
-        {0,         0,         0}
-    };
-    test_data_array[6] =
-    {
-        {15246,    120536, -645721},
-        {    2,  -11.5675,  3.6614}
-    };
-    test_data_array[7] =
-    {
-        {0, 1154526, -1125452},
-        {1,  0.9748,        0}
-    };
-    test_data_array[8] =
-    {
-        {1,     -2068,   1069156},
-        {1, 1034.0000, 1034.0000}
+        {{    1,                  4,                   -3},    {2,    -4.6458,    0.6458}},
+        {{    1,                  0,                   -4},    {2,    -2.0000,    2.0000}},
+        {{    0,                  0,                    0},    {8,          0,         0}},
+        {{    0,                  4,                    5},    {1,    -1.2500,         0}},
+        {{    0,                  0,                    5},    {0,          0,         0}},
+        {{    2,                  3,                    7},    {0,          0,         0}},
+        {{15246,             120536,              -645721},    {2,   -11.5675,    3.6614}},
+        {{    0,            1154526,             -1125452},    {1,     0.9748,         0}},
+        {{    1,              -2068,              1069156},    {1,  1034.0000, 1034.0000}},
+        {{    1,  3.4*pow(10.0, 20),              1231031},    {3,          0,         0}},
+        {{    1,                 10,   -3.4*pow(10.0, 38)},    {3,          0,         0}},
     };
     for(int i = 0; i < NUMBER_OF_TESTS; i++)
     {
@@ -203,3 +171,4 @@ int main()
     puts("roots belongs to R");
     return 0;
 }
+
