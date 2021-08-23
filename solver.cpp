b4b6ebeccf0b248ed499ea00f909084da743eaee
nik0zer <87236151+nik0zer@users.noreply.github.com>
2021-08-23 22:44:31 +0300

    Update solver.cpp

--- solver.cpp
+++ solver.cpp
@@ -1,9 +1,60 @@
 #include <stdio.h>
 #include <math.h>
+#define NDEBUG
 #include <assert.h>
 #include <float.h>
 #include <errno.h>
 #include "solver.h"
+
+void typeRootnum(int Rootnum)
+{
+    switch(Rootnum)
+    {
+    case NO_ROOT:
+        puts("no roots");
+        break;
+    case ONE_ROOT:
+        puts("one root");
+        break;
+    case TWO_ROOTS:
+        puts("two roots");
+        break;
+    case ERROR_OCCUR:
+        puts("failed with an error");
+        break;
+    case INFINITE_ROOTS:
+        puts("roots belongs to R");
+        break;
+    default:
+        puts("unknown root number");
+        break;
+    }
+}
+
+void typeErrors(int error)
+{
+    switch(error)
+    {
+    case NO_ERRORS:
+        puts("no errors");
+        break;
+    case VARIABLE_OVERFLOW:
+        puts("the value in the variable is too large");
+        break;
+    case NULL_POINTER:
+        puts("pointer = NULL");
+        break;
+    case EQUAL_POINTERS:
+        puts("two equal pointers");
+        break;
+    case NAN_INPUT:
+        puts("value of the variable is NAN");
+        break;
+    default:
+        puts("unknown error number");
+        break;
+    }
+}
 
 int floatcmp(float x1, float x2)
 {
@@ -14,7 +65,12 @@ int floatcmp(float x1, float x2)
     return 0;
 }
 
-float schet_linear(float b, float c)
+/*!
+\brief Находит корень линейного уравнения от переданных коэффициентов
+\param b,с коэффициенты
+\return корень
+*/
+static float schet_linear(float b, float c)
 {
     if(!floatcmp(c, 0))
         return float(0);
@@ -22,8 +78,16 @@ float schet_linear(float b, float c)
         return -c / b;
 }
 
-int schet_kvadr(float a, float b, float c, float* x1, float* x2)
+/*!
+\brief Находит корни квадратного уравнения от переданных коэффициентов
+\param a,b,с коэффициенты
+\param x1,x2 возвращаемые корни уравнения
+*/
+static int schet_kvadr(float a, float b, float c, float* x1, float* x2)
 {
+    assert(isfinite(b * b));
+    assert(isfinite(4 * a * c));
+    assert(isfinite(b * b - 4 * a * c));
     if(!isfinite(b * b) || !isfinite(4 * a * c) || !isfinite(b * b - 4 * a * c))
     {
         errno = VARIABLE_OVERFLOW;
@@ -45,6 +109,12 @@ int schet_kvadr(float a, float b, float c, float* x1, float* x2)
 
 int kvadratka(float a, float b, float c, float* x1, float* x2)
 {
+    assert(x1 != NULL);
+    assert(x2 != NULL);
+    assert(x1 != x2);
+    assert(!isnan(a));
+    assert(!isnan(b));
+    assert(!isnan(c));
     if(x1 == NULL || x2 == NULL)
     {
         errno = NULL_POINTER;
@@ -70,4 +140,27 @@ int kvadratka(float a, float b, float c, float* x1, float* x2)
         return ONE_ROOT;
     }
     return schet_kvadr(a, b, c, x1, x2);
+}
+
+int output(float a, float b, float c)
+{
+    float x1 = NAN, x2 = NAN;
+    int number_of_roots = kvadratka(a, b, c, &x1, &x2);
+    if(errno != 0)
+    {
+        printf("ERROR: %d\n", errno);
+        typeErrors(errno);
+        return errno;
+    }
+    if(number_of_roots != INFINITE_ROOTS)
+    {
+        printf("number of roots: %d\n", number_of_roots);
+        if(number_of_roots == ONE_ROOT)
+            printf("root: %.4f\n", x1);
+        if(number_of_roots == TWO_ROOTS)
+            printf("roots: %.4f %.4f\n", x1, x2);
+        return 0;
+    }
+    puts("roots belongs to R");
+    return 0;
 }
