10b8f4089b62ac3ac3db3e53e6dcb935cd401158
nik0zer <87236151+nik0zer@users.noreply.github.com>
2021-08-23 22:44:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ struct test_data
 {
     float data[3];
     float data_check[4];
+    int error;
     float* x1;
     float* x2;
 };
@@ -22,26 +23,26 @@ struct test_data
 */
 void test()
 {
-    float x1 = 0, x2 = 0;
+    float x1 = NAN, x2 = NAN;
     test_data test_data_array[] =
     {
-        {{    1,                  4,                   -3},    {     TWO_ROOTS,    -4.6458,    0.6458,         NO_ERRORS},    &x1,    &x2},
-        {{    1,                  0,                   -4},    {     TWO_ROOTS,    -2.0000,    2.0000,         NO_ERRORS},    &x1,    &x2},
-        {{    0,                  0,                    0},    {INFINITE_ROOTS,          0,         0,         NO_ERRORS},    &x1,    &x2},
-        {{    0,                  4,                    5},    {      ONE_ROOT,    -1.2500,         0,         NO_ERRORS},    &x1,    &x2},
-        {{    0,                  0,                    5},    {       NO_ROOT,          0,         0,         NO_ERRORS},    &x1,    &x2},
-        {{    2,                  3,                    7},    {       NO_ROOT,          0,         0,         NO_ERRORS},    &x1,    &x2},
-        {{15246,             120536,              -645721},    {     TWO_ROOTS,   -11.5675,    3.6614,         NO_ERRORS},    &x1,    &x2},
-        {{    0,            1154526,             -1125452},    {      ONE_ROOT,     0.9748,         0,         NO_ERRORS},    &x1,    &x2},
-        {{    1,              -2068,              1069156},    {      ONE_ROOT,  1034.0000, 1034.0000,         NO_ERRORS},    &x1,    &x2},
-        {{    1,  3.4*pow(10.0, 20),              1231031},    {   ERROR_OCCUR,          0,         0, VARIABLE_OVERFLOW},    &x1,    &x2},
-        {{    1,                 10,   -3.4*pow(10.0, 38)},    {   ERROR_OCCUR,          0,         0, VARIABLE_OVERFLOW},    &x1,    &x2},
-        {{    0,                  0,                    0},    {   ERROR_OCCUR,          0,         0,      NULL_POINTER},   NULL,    &x2},
-        {{    0,                  0,                    0},    {   ERROR_OCCUR,          0,         0,      NULL_POINTER},    &x1,   NULL},
-        {{    0,                  0,                    0},    {   ERROR_OCCUR,          0,         0,    EQUAL_POINTERS},    &x1,    &x1},
-        {{  NAN,                  0,                    0},    {   ERROR_OCCUR,          0,         0,         NAN_INPUT},    &x1,    &x2},
-        {{    0,                  0,                  NAN},    {   ERROR_OCCUR,          0,         0,         NAN_INPUT},    &x1,    &x2},
-        {{    1,      pow(10.0, 19),   -0.7*pow(10.0, 38)},    {   ERROR_OCCUR,          0,         0, VARIABLE_OVERFLOW},    &x1,    &x2}
+        {{    1,                  4,                   -3},    {     TWO_ROOTS,    -4.6458,    0.6458},         NO_ERRORS,    &x1,    &x2},
+        {{    1,                  0,                   -4},    {     TWO_ROOTS,    -2.0000,    2.0000},         NO_ERRORS,    &x1,    &x2},
+        {{    0,                  0,                    0},    {INFINITE_ROOTS,          0,         0},         NO_ERRORS,    &x1,    &x2},
+        {{    0,                  4,                    5},    {      ONE_ROOT,    -1.2500,         0},         NO_ERRORS,    &x1,    &x2},
+        {{    0,                  0,                    5},    {       NO_ROOT,          0,         0},         NO_ERRORS,    &x1,    &x2},
+        {{    2,                  3,                    7},    {       NO_ROOT,          0,         0},         NO_ERRORS,    &x1,    &x2},
+        {{15246,             120536,              -645721},    {     TWO_ROOTS,   -11.5675,    3.6614},         NO_ERRORS,    &x1,    &x2},
+        {{    0,            1154526,             -1125452},    {      ONE_ROOT,     0.9748,         0},         NO_ERRORS,    &x1,    &x2},
+        {{    1,              -2068,              1069156},    {      ONE_ROOT,  1034.0000, 1034.0000},         NO_ERRORS,    &x1,    &x2},
+        {{    1,  3.4*pow(10.0, 20),              1231031},    {   ERROR_OCCUR,          0,         0}, VARIABLE_OVERFLOW,    &x1,    &x2},
+        {{    1,                 10,   -3.4*pow(10.0, 38)},    {   ERROR_OCCUR,          0,         0}, VARIABLE_OVERFLOW,    &x1,    &x2},
+        {{    0,                  0,                    0},    {   ERROR_OCCUR,          0,         0},      NULL_POINTER,   NULL,    &x2},
+        {{    0,                  0,                    0},    {   ERROR_OCCUR,          0,         0},      NULL_POINTER,    &x1,   NULL},
+        {{    0,                  0,                    0},    {   ERROR_OCCUR,          0,         0},    EQUAL_POINTERS,    &x1,    &x1},
+        {{  NAN,                  0,                    0},    {   ERROR_OCCUR,          0,         0},         NAN_INPUT,    &x1,    &x2},
+        {{    0,                  0,                  NAN},    {   ERROR_OCCUR,          0,         0},         NAN_INPUT,    &x1,    &x2},
+        {{    1,      pow(10.0, 19),   -0.7*pow(10.0, 38)},    {   ERROR_OCCUR,          0,         0}, VARIABLE_OVERFLOW,    &x1,    &x2}
     };
     for(int i = 0; i < (sizeof(test_data_array) / sizeof(test_data_array[0])); i++)
     {
@@ -50,8 +51,9 @@ void test()
                                         test_data_array[i].data[2],
                                         test_data_array[i].x1,
                                         test_data_array[i].x2);
-        if(number_of_roots == int(test_data_array[i].data_check[0]) && number_of_roots == ERROR_OCCUR
-                && !floatcmp(float(errno), test_data_array[i].data_check[3]))
+        if(number_of_roots == int(test_data_array[i].data_check[0])
+        && number_of_roots == ERROR_OCCUR
+        && errno == test_data_array[i].error)
         {
             printf("test %i OK\n", i + 1);
             printf("ERROR %d\n", errno);
@@ -59,9 +61,9 @@ void test()
         else
         {
             if(number_of_roots == int(test_data_array[i].data_check[0])
-                    && !floatcmp(x1, test_data_array[i].data_check[1])
-                    && !floatcmp(x2, test_data_array[i].data_check[2])
-                    && !floatcmp(float(errno), test_data_array[i].data_check[3]))
+            && !floatcmp(x1, test_data_array[i].data_check[1])
+            && !floatcmp(x2, test_data_array[i].data_check[2])
+            && !floatcmp(float(errno), test_data_array[i].data_check[3]))
                 printf("test %i OK\n", i + 1);
             else
             {
@@ -82,7 +84,7 @@ void test()
 */
 int main()
 {
-    float a = NAN, b = NAN, c = NAN, x1 = NAN, x2 = NAN;
+    float a = NAN, b = NAN, c = NAN;
 #if TEST
     test();
     return 0;
@@ -93,22 +95,7 @@ int main()
         puts("invalid input");
         return 0;
     }
-    int number_of_roots = kvadratka(a, b, c, &x1, &x2);
-    if(errno != 0)
-    {
-        printf("ERROR: %d\n", errno);
-        return errno;
-    }
-    if(number_of_roots != INFINITE_ROOTS)
-    {
-        printf("number of roots: %d\n", number_of_roots);
-        if(number_of_roots == ONE_ROOT)
-            printf("root: %.4f\n", x1);
-        if(number_of_roots == TWO_ROOTS)
-            printf("roots: %.4f %.4f\n", x1, x2);
-        return 0;
-    }
-    puts("roots belongs to R");
-    return 0;
+    errno = 0;
+    output(a, b, c);
 #endif // TEST
 }
