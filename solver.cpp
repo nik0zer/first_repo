ad28b65c48ed280d495bca23d590fa5b431f71c1
nik0zer <87236151+nik0zer@users.noreply.github.com>
2021-08-23 17:21:45 +0300

    заменил abs, убрал комментарии

--- solver.cpp
+++ solver.cpp
@@ -5,16 +5,6 @@
 #include <errno.h>
 #include "solver.h"
 
-#define PRECISION 0.0001
-
-enum rootnum {NO_ROOT, ONE_ROOT, TWO_ROOTS, ERROR_OCCUR, INFINITE_ROOTS = 8};
-enum errors {NO_ERRORS, VARIABLE_OVERFLOW, NULL_POINTER, EQUAL_POINTERS, NAN_INPUT};
-
-/*!
-\brief Сравнивает 2 float
-\param x1,x2 числа для сравнения
-\return 0 - равны, 1 - первое больше, -1 - второе больше, аналогия с strcmp
-*/
 int floatcmp(float x1, float x2)
 {
     if((x1 - x2) > PRECISION)
@@ -24,11 +14,6 @@ int floatcmp(float x1, float x2)
     return 0;
 }
 
-/*!
-\brief Находит корень линейного уравнения от переданных коэффициентов
-\param b,с коэффициенты
-\return корень
-*/
 float schet_linear(float b, float c)
 {
     if(!floatcmp(c, 0))
@@ -37,14 +22,9 @@ float schet_linear(float b, float c)
         return -c / b;
 }
 
-/*!
-\brief Находит корни квадратного уравнения от переданных коэффициентов
-\param a,b,с коэффициенты
-\param x1,x2 возвращаемые корни уравнения
-*/
 int schet_kvadr(float a, float b, float c, float* x1, float* x2)
 {
-    if((abs(FLT_MAX / b) < abs(b)) || (abs((FLT_MAX / 4) / a) < abs(c)))
+    if((fabs(FLT_MAX / b) < fabs(b)) || (fabs((FLT_MAX / 4) / a) < fabs(c)))
     {
         errno = VARIABLE_OVERFLOW;
         return ERROR_OCCUR;
@@ -63,14 +43,6 @@ int schet_kvadr(float a, float b, float c, float* x1, float* x2)
     return floatcmp(d, 0) + 1;
 }
 
-/*!
-\brief Решает квадратное уравнение от переданных коэффициентов
-\detail Решает квадрратное уравнение разбивая на различные случаи в зависимости от входных данных квадратное, линейное и т.д
-\param a,b,с коэффициенты
-\param x1,x2 корни возвращаемые по итогам работы
-\warning при бесконечном количестве корней return возвращает 8, при ошибке возвращает 3
-\return количество корней
-*/
 int kvadratka(float a, float b, float c, float* x1, float* x2)
 {
     if(x1 == NULL || x2 == NULL)
