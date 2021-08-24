285f443d12826874f8fbb7e38a3f11ca301c265f
nik0zer <87236151+nik0zer@users.noreply.github.com>
2021-08-24 11:57:51 +0300

    пробелы, вывод ошибок, enum вместо 0

--- solver.cpp
+++ solver.cpp
@@ -5,53 +5,54 @@
 #include <float.h>
 #include <errno.h>
 #include "solver.h"
+#include <string.h>
 
-void typeRootnum(int Rootnum)
+void typeRootnum(int Rootnum, char* str)
 {
     switch(Rootnum)
     {
     case NO_ROOT:
-        puts("no roots");
+        sprintf(str, "no roots");
         break;
     case ONE_ROOT:
-        puts("one root");
+        sprintf(str, "one root");
         break;
     case TWO_ROOTS:
-        puts("two roots");
+        sprintf(str, "two roots");
         break;
     case ERROR_OCCUR:
-        puts("failed with an error");
+        sprintf(str, "failed with an error");
         break;
     case INFINITE_ROOTS:
-        puts("roots belongs to R");
+        sprintf(str, "roots belongs to R");
         break;
     default:
-        puts("unknown root number");
+        sprintf(str, "unknown root number");
         break;
     }
 }
 
-void typeErrors(int error)
+void typeErrors(int error, char* str)
 {
     switch(error)
     {
     case NO_ERRORS:
-        puts("no errors");
+        sprintf(str, "no errors");
         break;
     case VARIABLE_OVERFLOW:
-        puts("the value in the variable is too large");
+        sprintf(str, "the value in the variable is too large");
         break;
     case NULL_POINTER:
-        puts("pointer = NULL");
+        sprintf(str, "pointer = NULL");
         break;
     case EQUAL_POINTERS:
-        puts("two equal pointers");
+        sprintf(str, "two equal pointers");
         break;
     case NAN_INPUT:
-        puts("value of the variable is NAN");
+        sprintf(str, "value of the variable is NAN");
         break;
     default:
-        puts("unknown error number");
+        sprintf(str, "unknown error number");
         break;
     }
 }
@@ -82,24 +83,28 @@ static float schet_linear(float b, float c)
 \brief Находит корни квадратного уравнения от переданных коэффициентов
 \param a,b,с коэффициенты
 \param x1,x2 возвращаемые корни уравнения
+\return количество корней
 */
 static int schet_kvadr(float a, float b, float c, float* x1, float* x2)
 {
     assert(isfinite(b * b));
     assert(isfinite(4 * a * c));
     assert(isfinite(b * b - 4 * a * c));
+
     if(!isfinite(b * b) || !isfinite(4 * a * c) || !isfinite(b * b - 4 * a * c))
     {
         errno = VARIABLE_OVERFLOW;
         return ERROR_OCCUR;
     }
+
     float d = (b * b) - (4 * a * c);
     if(d < 0)
     {
         *x1 = 0;
         *x2 = 0;
-        return 0;
+        return NO_ROOT;
     }
+
     float change_part = sqrt(d) / (2 * a);
     float const_part = -b / (2 * a);
     *x1 = -change_part + const_part;
@@ -115,43 +120,53 @@ int kvadratka(float a, float b, float c, float* x1, float* x2)
     assert(!isnan(a));
     assert(!isnan(b));
     assert(!isnan(c));
+
     if(x1 == NULL || x2 == NULL)
     {
         errno = NULL_POINTER;
         return ERROR_OCCUR;
     }
+
     if(x2 == x1)
     {
         errno = EQUAL_POINTERS;
         return ERROR_OCCUR;
     }
+
     if(isnan(a) || isnan(b) || isnan(c))
     {
         errno = NAN_INPUT;
         return ERROR_OCCUR;
     }
+
     *x1 = 0;
     *x2 = 0;
+
     if(!floatcmp(a, 0) && !floatcmp(b, 0))
         return (!floatcmp(c, 0)) ? INFINITE_ROOTS : NO_ROOT;
+
     if(!floatcmp(a, 0))
     {
         *x1 = schet_linear(b, c);
         return ONE_ROOT;
     }
+
     return schet_kvadr(a, b, c, x1, x2);
 }
 
 int output(float a, float b, float c)
 {
     float x1 = NAN, x2 = NAN;
+    char str[50];
     int number_of_roots = kvadratka(a, b, c, &x1, &x2);
     if(errno != 0)
     {
         printf("ERROR: %d\n", errno);
-        typeErrors(errno);
+        typeErrors(errno, str);
+        printf("%s", str);
         return errno;
     }
+
     if(number_of_roots != INFINITE_ROOTS)
     {
         printf("number of roots: %d\n", number_of_roots);
